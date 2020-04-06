ef9ab25dd956c3db3aad99b9b1857c44f431c4a8
MadRajib <madhabsharma94@gmail.com>
2020-04-06 22:09:55 +0530

    sd

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,9 +18,7 @@
      * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
      */
     function activateTab() {
-      var lastPath = "/"+window.location.pathname.split("/").pop();
-
-      if(['/tags.html', '/categories.html'].indexOf(lastPath) > -1) {
+      if(window.location.pathname.endsWith('/categories.html') || window.location.pathname.endsWith('/tags.html') ){
       
         var hash = window.location.hash;    
         if(hash)
